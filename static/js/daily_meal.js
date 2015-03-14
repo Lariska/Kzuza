7f43aa06bfc7d9deff8329a6d830c773748553b8
lariska <alex@Alexanders-MacBook-Pro.local>
2015-03-14 18:00:42 +0200

    adding slider

---
+++ static/js/daily_meal.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+        slideWidth: 1000,
+        minSlides: 1,
+        maxSlides: 1,
+        auto: true,
+        slideMargin: 10
+    });
+});
