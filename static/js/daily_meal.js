e0ef63fdc624c47bed339f5dacf798c292d52713
lariska <alex@Alexanders-MacBook-Pro.local>
2015-03-18 21:21:39 +0200

    fixing daily_meal page

--- static/js/daily_meal.js
+++ static/js/daily_meal.js
@@ -1,9 +1,7 @@
-$(document).ready(function(){
-    $('.slider1').bxSlider({
-        slideWidth: 1000,
-        minSlides: 1,
-        maxSlides: 1,
-        auto: true,
-        slideMargin: 10
-    });
+$('.slider1').bxSlider({
+    slideWidth: 1000,
+    minSlides: 1,
+    maxSlides: 1,
+    auto: true,
+    slideMargin: 10
 });
